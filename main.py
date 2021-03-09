d9263771f727fbfecc5aee06632b0e25df6afb95
Matthew Arras <Matthew.arras@gmail.com>
2021-03-08 20:41:23 -0800

    Tested both datasets

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pandas as pd
 
 def main():
-    
+    regs_18 = pd.read_csv('regular_stats_18_19.csv')
+    regs_19 = pd.read_csv('regular_stats_19_20.csv')
+
+    print(regs_18['Team'])
+    print(regs_18['Team'])
 
 
 if __name__ == '__main__':
