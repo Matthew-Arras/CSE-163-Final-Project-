9446717deeef6b883c772400a03555a841dfb033
Matthew Arras <Matthew.arras@gmail.com>
2021-03-16 01:20:53 -0700

    Started scrapping

--- bmain.py
+++ bmain.py
@@ -2,9 +2,9 @@ import pandas as pd
 import os
 from bs4 import BeautifulSoup
 import requests
-from selenium import webdriver
-import lxml.html as lh
-import json
+# from selenium import webdriver
+# import lxml.html as lh
+# import json
 
 
 def main():
@@ -16,13 +16,27 @@ def main():
 
     #Retreiving URL from basketball-reference for 2018-2019 regular season stats
     url = "https://www.basketball-reference.com/leagues/NBA_2019.html"
+
     #Retrieves webpage content
     source = requests.get(url, headers= headers)
     #Create soup object using source(webpage) content and paring with lxml
     soup = BeautifulSoup(source.content, 'html.parser')
-    example = soup.find_all('tr', {"id"="div_team-stats-per_game"})
-    print(example)
+
+    # up for unpeel
+    up1 = soup.find(id='all_team-stats-per_game')
+
+    up2 = BeautifulSoup(up1.contents[4], 'html.parser')
+    up3 = up2.find('table')
+    print(up3)
+
+    # Process of getting column headers
+    col_head_row = up3.contents[5].tr
     
+    # Column headers for regular stats
+    headers = [th.get_text() for th in col_head_row.find_all('th')]
+    headers = headers[1:]
+   
+
 
 
 if __name__ == '__main__':
