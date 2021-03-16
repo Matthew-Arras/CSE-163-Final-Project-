4fa2f8da78c9cddeb7802b9d633040e9be856294
soriabjo <soriabjo@uw.edu>
2021-03-16 14:03:43 -0700

    ML analysis update(with string/float error)

--- ml_analysis.py
+++ ml_analysis.py
@@ -5,7 +5,9 @@ import pandas as pd
 import data_prep
 
 
-def training_labels_wl_column(regs_18):
+#Returns training label CSV with W/L% column using traditional basketball
+#statistics for the 2018-2019 regular season 
+def training_labels_wl_column_tr(regs_18):
     """
     Creates a Win / Loss percentage column by creating 
     a games played column ('G') by adding the 'W' and 'L'
@@ -18,158 +20,132 @@ def training_labels_wl_column(regs_18):
     training_labels['W/L%'] = training_labels['W']/training_labels['G']
     
     training_new = training_labels[['Team', 'W/L%']]
-    training_new = training_new['Team'].str.replace('*', '')
-    print(training_new)
-    training_final = regs_18.merge(training_new, on='Team', how='left')
-    print(training_final)
+    training_new['Team'] = training_new['Team'].str.replace('*', '')
+
+    training_final = regs_18.merge(training_new, on='Team', how='outer')
     return training_final
- 
-#def fit_and_predict_wp_tr_18(wl_percentage, regs_18):
+
+
+#Returns 2019-2020 regular season traditional statistics data table with W/L percentage column
+#using 19-20 testing label CSV in order to be used later in ML model functions for predicting WL
+#percentages using traditional statisitcs.
+def testing_labels(regs_19):
+    testing_labels_feat = pd.read_csv(r"https://raw.githubusercontent.com/Matthew-Arras/CSE-163-Final-Project-/main/data/19-20_testing%20_labels.csv")
+    testing_labels_feat = testing_labels_feat.dropna()
+    testing_labels_feat['G'] = testing_labels_feat['W'] + testing_labels_feat['L']
+    testing_labels_feat['W/L%'] = testing_labels_feat['W'] / testing_labels_feat['G']
+
+    testing_labels_new = testing_labels_feat[['Team', 'W/L%']]
+    testing_labels_new['Team'] = testing_labels_new['Team'].str.replace('*', '')
+
+    testing_labels_final = regs_19.merge(testing_labels_new, on='Team', how='outer')
+    return testing_labels_final
+
+
+#Returns training label CSV with W/L percentage (using advanced basketball statistics) column to be used for declaring features
+#in ML model functions
+def training_labels_wl_column_adv(advs_18):
+    training_labels_adv = pd.read_csv(r"https://raw.githubusercontent.com/Matthew-Arras/CSE-163-Final-Project-/main/data/18-19_training_labels.csv")
+    training_labels_adv = training_labels_adv.dropna()
+    training_labels_adv['G'] = training_labels_adv['W'] + training_labels_adv['L']
+    training_labels_adv['W/L%'] = training_labels_adv['W']/training_labels_adv['G'] 
+
+    training_new_adv = training_labels_adv[['Team', 'W/L%']]
+    training_new_adv['Team'] = training_new_adv['Team'].str.replace('*', '')
+
+    training_adv_final = advs_18.merge(training_new_adv, on='Team', how='outer')
+    return training_adv_final
+
+
+#Returns 2019-2020 regular season advanced statistics data table with W/L percentage column
+#using 19-20 testing label CSV in order to be used later in ML model functions for predicting WL
+#percentages using traditional statisitcs.
+def testing_labels_adv(advs_19):
+    testing_labels_feat_adv = pd.read_csv(r"https://raw.githubusercontent.com/Matthew-Arras/CSE-163-Final-Project-/main/data/19-20_testing%20_labels.csv")
+    testing_labels_feat_adv = testing_labels_feat_adv.dropna()
+    testing_labels_feat_adv['G'] = testing_labels_feat_adv['W'] + testing_labels_feat_adv['L']
+    testing_labels_feat_adv['W/L%'] = testing_labels_feat_adv['W'] / testing_labels_feat_adv['G']
+
+    testing_labels_new_adv = testing_labels_feat_adv[['Team', 'W/L%']]
+    testing_labels_new_adv['Team'] = testing_labels_new_adv['Team'].str.replace('*', '')
+
+    testing_labels_final_adv = advs_19.merge(testing_labels_new_adv, on='Team', how='outer')
+    return testing_labels_final_adv
+
+
+def fit_and_predict_wp_tr_18(wl_percentage, testing_features):
     """
     Constructs a ML Regression Model that predicts the win
     percentages of NBA teams using traditional statistics
-    from the 2018-2019 regular season
-        parameters:
-            data: The pandas dataframe that 
-                contains NBA regular season stats.
+    from the 2018-2019 regular season to the 2019-2020
+    regular season.
     """
     #Grab all columns except for win/loss percentage (W/L%) for
     #features
-    #features = regs_18.loc[:, :]
+    features = wl_percentage.loc[:, wl_percentage.columns != 'W/L%']
  
     #Set the label/column we are predicting on to 
     #win/loss percentage (W/L%)
-    #labels = wl_percentage['W/L%']
+    labels = wl_percentage['W/L%']
  
  
     #Create untrained model
-    #model = DecisionTreeRegressor()
+    model = DecisionTreeRegressor()
  
     #Train model on training set
-    #model.fit(features_train, labels_train)
+    train_pred = model.fit(features, labels)
  
-    #test_pred = model.predict(features_test)
+    test_pred = model.predict(testing_features)
 
 
     #Calculates the accuracy of predictions using mean squared 
     #error of test set
-    #test_acctual = mean_squared_error(labels_test, test_pred)
-    #print('Traditional 18-19 WL Prediction')
-    #print(test_acctual)
-    #return test_acctual
+    test_acctual = mean_squared_error(train_pred, test_pred)
+    print('Traditional 18-19 WL Prediction for 19-20 Accuracy')
+    print(test_acctual)
+    return test_acctual
 
 
-#def fit_and_predict_wp_adv_18(wl_percentage, advs_18, regs_18):
+def fit_and_predict_wp_adv_18(wl_percentage_adv, testing_features_adv):
     """
     Constructs a ML Regression Model that predicts the win
-    percentages of NBA teams using advanced statistics from
-    the 2018-2019 regular season
-        parameters:
-            data: The pandas dataframe that 
-                contains NBA regular season stats.
+    percentages of NBA teams using advanced statistics
+    from the 2018-2019 regular season to the 2019-2020
+    regular season.
     """
     #Grab all columns except for win/loss percentage (W/L%) for
     #features
-    #features = regs_18
+    features = wl_percentage_adv[:, wl_percentage_adv.columns != 'W/L%']
  
     #Set the label/column we are predicting on to 
     #win/loss percentage (W/L%)
-    #labels = wl_percentage['W/L%']
+    labels = wl_percentage_adv['W/L%']
  
  
     #Create untrained model
-    #model = DecisionTreeRegressor()
+    model = DecisionTreeRegressor()
  
     #Train model on training set
-    #model.fit(regs_18)
+    train_pred_adv = model.fit(features, labels)
  
-    #test_pred = model.predict(features_test)
+    test_pred_adv = model.predict(testing_features_adv)
 
 
     #Calculates the accuracy of predictions using mean squared 
     #error of test set
-    #test_acctual = mean_squared_error(labels_test, test_pred)
-    #print('Advanced 18-19 WL Prediction')
-    #print(test_acctual)
-    #return test_acctual
+    test_acctual = mean_squared_error(train_pred_adv, test_pred_adv)
+    print('Advanced 18-19 WL Prediction')
+    print(test_acctual)
+    return test_acctual
 
 
-#def fit_and_predict_wp_adv_19(wl_percentage, advs_19, regs_18):
-    """
-    Constructs a ML Regression Model that predicts the win
-    percentages of NBA teams using advanced statistics from
-    the 2019-2020 regular season
-        parameters:
-            data: The pandas dataframe that 
-                contains NBA regular season stats.
-    """
-    #Grab all columns except for win/loss percentage (W/L%) for
-    #features
-    #features = advs_19.loc[:, :]
- 
-    #Set the label/column we are predicting on to 
-    #win/loss percentage (W/L%)
-    #labels = wl_percentage['W/L%']
- 
-    #Create untrained model
-    #model = DecisionTreeRegressor()
- 
-    #Train model on training set
-    #model.fit(regs_18)
- 
-    #test_pred = model.predict(features_test)
-
-
-    #Calculates the accuracy of predictions using mean squared 
-    #error of test set
-    #test_acctual = mean_squared_error(labels_test, test_pred)
-    #print('Advanced 19-20 WL Prediction')
-    #print(test_acctual)
-    #return test_acctual
-
- 
-#def fit_and_predict_wp_tr_19(wl_percentage, regs_19, regs_18):
-    """
-    Constructs a ML Regression Model that predicts the win
-    percentages of NBA teams using traditional statistics from
-    the 2019-2020 regular season
-        parameters:
-            data: The pandas dataframe that 
-                contains NBA regular season stats.
-    """
-    #Grab all columns except for win/loss percentage (W/L%) for
-    #features
-    #features = regs_19.loc[:, :]
-
-    #Set the label/column we are predicting on to 
-    #win/loss percentage (W/L%)
-    #labels = wl_percentage['W/L%']
- 
-    #Create untrained model
-    #model = DecisionTreeRegressor()
- 
-    #Train model on training set
-    #model.fit(features_train, labels_train)
- 
-    #test_pred = model.predict(features_test)
-
-
-    #Calculates the accuracy of predictions using mean squared 
-    #error of test set
-    #test_acctual = mean_squared_error(labels_test, test_pred)
-    #print('Traditional 19-20 WL Prediction')
-    #print(test_acctual)
-    #return test_acctual
-
-
-#def comparison(tr18, tr19, adv18, adv19):
-    #if tr18 < adv18 and tr19 < adv19:
-        #print('Advanced Statisitcs performed better!')
-    #elif tr18 > adv18 and tr19 > adv19:
-        #print('Traditional Statistics performed better!')
-    #else:
-        #print('Unable to determine outcome predicitons')
-
+def comparison(tr18, adv18):
+    if tr18 < adv18:
+        print('Advanced Statistics predicts W/L percentages better')
+    elif adv18 > tr18:
+        print('Traditional Statistics predicts W/L percentages better')
+    else:
+        print('Neither basketball statitic category outperforsm the outerh - tie.')
 
 def main():
     regs_18 = data_prep.scrape_regular('https://www.basketball-reference.com/leagues/NBA_2019.html')
@@ -178,17 +154,18 @@ def main():
     advs_18 = data_prep.scrape_advanced('https://www.basketball-reference.com/leagues/NBA_2019.html')
     advs_19 = data_prep.scrape_advanced('https://www.basketball-reference.com/leagues/NBA_2020.html')
 
-    wl_percentage = training_labels_wl_column(regs_18)
+    wl_percentage = training_labels_wl_column_tr(regs_18)
+    testing_features = testing_labels(regs_19)
 
-    #tr18 = fit_and_predict_wp_tr_18(wl_percentage, regs_18)
-    #print()
-    #adv18 = fit_and_predict_wp_adv_18(wl_percentage, advs_18, regs_18)
-    #print()
-    #tr19 = fit_and_predict_wp_tr_19(wl_percentage, regs_19, regs_18)
-    ##print()
-    #adv19 = fit_and_predict_wp_adv_19(wl_percentage, advs_19, regs_18)
-    #print()
-    #comparison(tr18, tr19, adv18, adv19)
+    wl_percentage_adv = training_labels_wl_column_adv(advs_18)
+    testing_features_adv = testing_labels_adv(advs_19)
+
+    tr18 = fit_and_predict_wp_tr_18(wl_percentage, testing_features)
+    print()
+    adv18 = fit_and_predict_wp_adv_18(wl_percentage_adv, testing_features_adv)
+
+    comparison(tr18, adv18)
+    print('---END OF FILE--')
  
  
 if __name__ == '__main__':
