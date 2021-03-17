18aa5fbfd2392aa8a34899e0e912327578faa0d5
Matthew Arras <Matthew.arras@gmail.com>
2021-03-16 18:06:55 -0700

    A

--- ml_analysis.py
+++ ml_analysis.py
@@ -1,5 +1,4 @@
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 import pandas as pd
 import data_prep
