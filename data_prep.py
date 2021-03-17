a4050e9fedec829e0dc63a6ef8d5d2c34401d6bd
Matthew Arras <Matthew.arras@gmail.com>
2021-03-16 20:17:57 -0700

    flake8

--- data_prep.py
+++ data_prep.py
@@ -1,15 +1,14 @@
 '''
-Matthew Arras and Bjorn Soriano 
-CSE 163 Winter Quarter 
-Contains functions that scrap advanced and traditional 
+Matthew Arras and Bjorn Soriano
+CSE 163 Winter Quarter
+Contains functions that scrap advanced and traditional
 statistics off of basketball-reference.com
 '''
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
 
-# As of now takes in a URL and returns a pandas dataframe 
-# of the traditional stats
+
 def scrape_regular(url):
     '''
     Takes in the url of a page on basketball reference and
@@ -18,9 +17,9 @@ def scrape_regular(url):
     '''
     w_headers = {'User-Agent': 'Mozilla/5.0'}
 
-    #Retrieves webpage content
+    # Retrieves webpage content
     source = requests.get(url, headers=w_headers)
-    #Create soup object using source(webpage) content and paring with lxml
+    # Create soup object using source(webpage) content and paring with lxml
     soup = BeautifulSoup(source.content, 'html.parser')
 
     # up for unpeel
@@ -28,11 +27,10 @@ def scrape_regular(url):
 
     up2 = BeautifulSoup(up1.contents[4], 'html.parser')
     up3 = up2.find('table')
-    
 
     # Process of getting column headers
     col_head_row = up3.contents[5].tr
-    
+
     # Column headers for regular stats
     headers = [th.get_text() for th in col_head_row.find_all('th')]
     headers = headers[1:]
@@ -40,15 +38,15 @@ def scrape_regular(url):
     # Process of getting actual team data
     raw_data = up3.contents[7]
     data_rows = raw_data.find_all('tr')
-    
-    team_stats = [[td.getText() for td in data_rows[i].findAll('td')]
-            for i in range(len(data_rows))]
-    
-    r_stats = pd.DataFrame(team_stats, columns = headers)
 
-    #Temporarily cut off team name
+    team_stats = [[td.getText() for td in data_rows[i].findAll('td')]
+                   for i in range(len(data_rows))]
+
+    r_stats = pd.DataFrame(team_stats, columns=headers)
+
+    # Temporarily cut off team name
     names = r_stats['Team']
-    
+
     r_stats = r_stats.loc[:, 'G':'PTS']
 
     for cname in r_stats.columns:
@@ -56,9 +54,8 @@ def scrape_regular(url):
 
     r_stats['Team'] = names
 
-    #Join win percs to r_stats
-    #Cut unneccesary data off of r stats 
-    #Make sure all values that should be numerical are
+    # Join win percs to r_stats
+    # Cut unneccesary data off of r stats
     return r_stats
 
 
@@ -70,9 +67,10 @@ def scrape_advanced(url):
     '''
     w_headers = {'User-Agent': 'Mozilla/5.0'}
 
-    #Retrieves webpage content
+    # Retrieves webpage content
     source = requests.get(url, headers=w_headers)
-    #Create soup object using source(webpage) content and paring with lxml
+
+    # Create soup object using source(webpage) content and paring with lxml
     soup = BeautifulSoup(source.content, 'html.parser')
 
     # up for unpeel
@@ -80,20 +78,20 @@ def scrape_advanced(url):
 
     up2 = BeautifulSoup(up1.contents[4], 'html.parser')
     up3 = up2.find('table')
-    
+
     # Process of getting column headers
     col_head_row = up3.contents[5].contents[3]
 
-    # Column headers 
+    # Column headers
     headers = [th.get_text() for th in col_head_row.find_all('th', limit=21)]
     headers = headers[1:]
 
     # Process of getting actual team data
     raw_data = up3.contents[7]
     data_rows = raw_data.find_all('tr')
-    
+
     team_stats = [[td.getText() for td in data_rows[i].findAll('td', limit=20)]
-                for i in range(len(data_rows))]
+                  for i in range(len(data_rows))]
 
     a_stats = pd.DataFrame(team_stats, columns=headers)
 
